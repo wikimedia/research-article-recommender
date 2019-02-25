e06cf0abcbe8c413fb48e531f6e187f9c6263b2a
Baha <bmansurov@wikimedia.org>
2019-02-25 10:53:04 -0500

    Add packaging instructions
    Bug: T211981
    Change-Id: Ic5fb30eb446445eec5fc0a45c513e6a29755e90e

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.org", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="article-recommender-bmansurov",
+    version="0.0.1",
+    author="Bahodir Mansurov",
+    author_email="bmansurov@wikimedia.org",
+    description="article normalized scores generator",
+    long_description=long_description,
+    long_description_content_type="text/x-org",
+    url="https://gerrit.wikimedia.org/r/#/admin/projects/research/article-recommender",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+)
