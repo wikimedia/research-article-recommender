9f1746ca0ac510b09d1b534cb6f222309ccbfe02
Baha <bmansurov@wikimedia.org>
2019-03-01 15:37:22 -0500

    Make content ready for upload to PyPi
    Change-Id: I988e7982074bf0356a64d2252c9590e7114c24b3

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-with open("README.org", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="article_recommender-bmansurov",
-    version="0.0.1",
+    version="0.0.2.1",
     author="Bahodir Mansurov",
     author_email="bmansurov@wikimedia.org",
     description="article normalized scores generator",
     long_description=long_description,
-    long_description_content_type="text/x-org",
+    long_description_content_type="text/markdown",
     url="https://gerrit.wikimedia.org/r/#/admin/projects/research/article-recommender",
     packages=['article_recommender'],
     package_dir={'article_recommender': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: OS Independent",
     ],
 )
